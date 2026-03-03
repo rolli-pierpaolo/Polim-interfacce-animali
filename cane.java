b40215f7eb183b6fcff669705674ff5109aa2c7b
Pierpaolo Rolli <pierolli1996@gmail.com>
2026-03-03 10:11:16 +0100

    Create cane.java

---
+++ cane.java
@@ -0,0 +1,16 @@
+package animals;
+
+public class Cane extends Animali{
+    
+   public Cane(String nome) {
+       super(nome);
+   }
+  
+   //@override
+   
+   public void  verso() {
+       System.out.println("Il cane abbaia");
+   }
+   
+   
+}
